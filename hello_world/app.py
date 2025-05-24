ba0e4d07dc6aca80a9ffae0ebbae729b90494c46
GuillermoLB <guillelb99@gmail.com>
2025-05-24 21:58:58 +0200

    Update pipeline.yaml to run unit tests and install dependencies; fix docstring format in lambda_handler

--- hello_world/app.py
+++ hello_world/app.py
@@ -20,7 +20,7 @@ def lambda_handler(event, context):
 
     Returns
     ------
-    API Gateway Lambda Proxy Output Format: dict
+    API Gateway Lambda Proxy Output Format: dictdd
 
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
